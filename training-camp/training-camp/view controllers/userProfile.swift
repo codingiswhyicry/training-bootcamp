2ef6b7a486f1dcb9ce2ce68b0cb613528404a21d
thecodingone <amanda.southworth@myds.us>
2017-07-08 10:34:50 -0700

    added files to .xcodeproj

---
+++ training-camp/training-camp/view controllers/userProfile.swift
@@ -0,0 +1,35 @@
+//
+//  userProfile.swift
+//  training-camp
+//
+//  Created by Amanda Southworth on 7/8/17.
+//  Copyright © 2017 Amanda Southworth. All rights reserved.
+//
+
+import UIKit
+
+class userProfile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
